ffc4dacb6ecba92d88d69e6326f5325c2038b4a0
frandrew90 <frandrewjs@gmail.com>
2022-07-11 21:07:01 +0300

    added styles for Sidebar and App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Main from '../Main/Main';
 import Sidebar from '../Sidebar/Sidebar';
+import './App.css';
 
 const App = () => {
   return (
-    <>
+    <div className="main-container">
       <Sidebar />
       <Main />
-    </>
+    </div>
   );
 };
 
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import Navigation from '../Navigation/Navigation';
+import './Sidebar.css';
+
+const defineStyles = isOpen => {
+  const finalStyles = ['Sidebar'];
+
+  if (!isOpen) {
+    finalStyles.push('Sidebar-closed');
+  }
+  return finalStyles.join(' ');
+};
 
 const Sidebar = () => {
+  const isOpen = false;
   return (
-    <div>
+    // <div className={`Sidebar ${isOpen ? '' : 'Sidebar-closed'}`}>
+    <div className={defineStyles(isOpen)}>
+      <div className="Sidebar-decor"></div>
+      <button className="toggle-btn" aria-label="Toggle sidebar"></button>
       <Navigation />
     </div>
   );
