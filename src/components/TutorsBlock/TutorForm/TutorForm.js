e55bcca165db69f0447de7bc3d15a103c0a35718
frandrew90 <frandrewjs@gmail.com>
2022-07-15 00:01:35 +0300

    added form

---
+++ src/components/TutorsBlock/TutorForm/TutorForm.js
@@ -0,0 +1,57 @@
+// import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import BigButton from '../../common/BigButton/BigButton';
+import Paper from '../../common/Paper/Paper';
+import s from './TutorForm.module.css';
+
+class TutorForm extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Paper>
+          <div className={s.inner}>
+            <h4 className="formName">Add Tutor</h4>
+            <form onSubmit={this.handleSubmit}>
+              <input
+                name="lastName"
+                type="text"
+                placeholder="Last name"
+                required
+              />
+              <input
+                name="firstName"
+                type="text"
+                placeholder="First name"
+                required
+              />
+              <input
+                name="patronimyc"
+                type="text"
+                placeholder="Patronimyc"
+                required
+              />
+              <input
+                name="phone"
+                type="tel"
+                placeholder="Phone number"
+                required
+              />
+              <input name="email" type="email" placeholder="Email" required />
+              <input name="city" type="text" placeholder="City" required />
+              <input
+                name="options"
+                type="text"
+                placeholder="Kind of activity"
+                required
+              />
+
+              <BigButton type="submit" text="Invite" disabled />
+            </form>
+          </div>
+        </Paper>
+      </div>
+    );
+  }
+}
+
+export default TutorForm;
--- src/components/TutorsBlock/TutorsBlock.js
+++ src/components/TutorsBlock/TutorsBlock.js
@@ -1,31 +1,66 @@
 /** @jsxImportSource @emotion/react */
 
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Tutor from './Tutor/Tutor';
-// import tutorIcon from '../../images/user-tie.svg';
-import addIcon from '../../images/plus.svg';
 import BigButton from '../common/BigButton/BigButton';
 import Paper from '../common/Paper/Paper';
+import TutorForm from './TutorForm/TutorForm';
+import addIcon from '../../images/plus.svg';
+import cancelIcon from '../../images/cancel-circle.svg';
 
-const TutorsBlock = ({ tutors = [] }) => {
-  return (
-    <div css={{ position: 'relative', marginBottom: '32px' }}>
-      {/* <img src={tutorIcon} alt="Tutors" />
-      <h3 className="heading">Tutors</h3> */}
-      <ul>
-        {tutors.map(tutor => (
-          <li key={tutor.email} css={{ marginBottom: '24px' }}>
-            <Paper>
-              <Tutor tutor={tutor} />
-            </Paper>
-          </li>
-        ))}
-      </ul>
-      <BigButton icon={addIcon} text="Add Tutor" />
-    </div>
-  );
-};
+// const TutorsBlock = ({ tutors = [] }) => {
+//   return (
+//     <div css={{ position: 'relative', marginBottom: '32px' }}>
+//       <ul>
+//         {tutors.map(tutor => (
+//           <li key={tutor.email} css={{ marginBottom: '24px' }}>
+//             <Paper>
+//               <Tutor tutor={tutor} />
+//             </Paper>
+//           </li>
+//         ))}
+//       </ul>
+//       <BigButton icon={addIcon} text="Add Tutor" />
+//     </div>
+//   );
+// };
+
+class TutorsBlock extends Component {
+  state = {
+    isFormOpen: false,
+  };
+
+  toggleForm = () => {
+    this.setState(prevState => ({
+      isFormOpen: !prevState.isFormOpen,
+    }));
+  };
+
+  render() {
+    return (
+      <div css={{ position: 'relative', marginBottom: '32px' }}>
+        <ul>
+          {this.props.tutors.map(tutor => (
+            <li key={tutor.email} css={{ marginBottom: '24px' }}>
+              <Paper>
+                <Tutor tutor={tutor} />
+              </Paper>
+            </li>
+          ))}
+        </ul>
+
+        {this.state.isFormOpen && <TutorForm />}
+
+        <BigButton
+          icon={this.state.isFormOpen ? cancelIcon : addIcon}
+          text={this.state.isFormOpen ? 'Cancel' : 'Add Tutor'}
+          onClickBtn={this.toggleForm}
+        />
+      </div>
+    );
+  }
+}
 
 TutorsBlock.propTypes = {
   tutors: PropTypes.arrayOf(
