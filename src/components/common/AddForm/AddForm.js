c9980e3c2ec228f24e5da4cb3357274420c8ad22
frandrew90 <frandrewjs@gmail.com>
2022-08-03 22:38:43 +0300

    refactoried AddForm

--- src/components/common/AddForm/AddForm.js
+++ src/components/common/AddForm/AddForm.js
@@ -1,82 +1,94 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import BigButton from '../../common/BigButton/BigButton';
 import Paper from '../../common/Paper/Paper';
 import s from './AddForm.module.css';
 
-class AddForm extends Component {
-  state = { city: '' };
+const AddForm = ({ onSubmit, formName, placeholder }) => {
+  const [input, setInput] = useState('');
 
-  // ==============================================================
+  const handleChange = e => setInput(e.target.value);
 
-  // componentDidMount() {
-  //   console.log('componentDidMount');
-  //   // this.setState() - можно вызывать
-  // }
-
-  // componentDidUpdate(prevProps, prevState, snapshot) {
-  //   console.log('componentDidUpdate');
-
-  //   console.log('prevState', prevState);
-  //   console.log('this.state', this.state);
-  //   // console.log('~snapshot', snapshot);
-  //   // Никогда не обновляем state без проверки
-  //   // this.setState() - можно вызывать
-  // }
-
-  // componentWillUnmount() {
-  //   console.log('componentWillUnmount');
-  //   // this.setState() - НЕЛЬЗЯ вызывать, только чистим сеттаймауті и тд и тп
-  // }
-  // ==============================================================
-
-  handleChange = e => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-
-    this.props.onSubmit(this.state.city);
-    this.reset();
+    onSubmit(input);
+    reset();
   };
 
-  reset = () => {
-    this.setState({ city: '' });
-  };
+  const reset = () => setInput('');
 
-  render() {
-    const { city } = this.state;
-    const { formName, placeholder } = this.props;
+  return (
+    <div className={s.container}>
+      <Paper>
+        <div className={s.inner}>
+          <h4 className="formName"> {formName}</h4>
+          <form onSubmit={handleSubmit}>
+            <input
+              value={input}
+              type="text"
+              placeholder={placeholder}
+              required
+              onChange={handleChange}
+            />
 
-    const isAddBtnDisabled = Object.values(this.state).some(
-      value => typeof value !== 'boolean' && !value,
-    );
+            <BigButton type="submit" text="Add" disabled={!input} />
+          </form>
+        </div>
+      </Paper>
+    </div>
+  );
+};
 
-    return (
-      <div className={s.container}>
-        <Paper>
-          <div className={s.inner}>
-            <h4 className="formName"> {formName}</h4>
-            <form onSubmit={this.handleSubmit}>
-              <input
-                name="city"
-                value={city}
-                type="text"
-                placeholder={placeholder}
-                required
-                onChange={this.handleChange}
-              />
+// class AddForm extends Component {
+//   state = { city: '' };
 
-              <BigButton type="submit" text="Add" disabled={isAddBtnDisabled} />
-            </form>
-          </div>
-        </Paper>
-      </div>
-    );
-  }
-}
+//   handleChange = e => {
+//     const { name, value } = e.target;
+//     this.setState({ [name]: value });
+//   };
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+
+//     this.props.onSubmit(this.state.city);
+//     this.reset();
+//   };
+
+//   reset = () => {
+//     this.setState({ city: '' });
+//   };
+
+//   render() {
+//     const { city } = this.state;
+//     const { formName, placeholder } = this.props;
+
+//     const isAddBtnDisabled = Object.values(this.state).some(
+//       value => typeof value !== 'boolean' && !value,
+//     );
+
+//     return (
+//       <div className={s.container}>
+//         <Paper>
+//           <div className={s.inner}>
+//             <h4 className="formName"> {formName}</h4>
+//             <form onSubmit={this.handleSubmit}>
+//               <input
+//                 name="city"
+//                 value={city}
+//                 type="text"
+//                 placeholder={placeholder}
+//                 required
+//                 onChange={this.handleChange}
+//               />
+
+//               <BigButton type="submit" text="Add" disabled={isAddBtnDisabled} />
+//             </form>
+//           </div>
+//         </Paper>
+//       </div>
+//     );
+//   }
+// }
 
 AddForm.propTypes = {
   onSubmit: PropTypes.func,
