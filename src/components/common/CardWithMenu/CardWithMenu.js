1523530b8865cf30a6627f9476eb64c1f848a824
frandrew90 <frandrewjs@gmail.com>
2022-08-03 15:53:46 +0300

    redactoring of CardWithMenu component

--- src/components/common/CardWithMenu/CardWithMenu.js
+++ src/components/common/CardWithMenu/CardWithMenu.js
@@ -1,72 +1,55 @@
 // /** @jsxImportSource @emotion/react */
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import randomIcon from '../../../images/make-group.svg';
 import editIcon from '../../../images/pencil.svg';
 import deleteIcon from '../../../images/bin.svg';
-// import { itemStyles } from '../../../styles/listStyles';
 import s from './CardWithMenu.module.css';
 
-class CardWithMenu extends Component {
-  state = {
-    isMenuOpen: false,
+const CardWithMenu = ({ text, onEdit, onDelete }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => setIsMenuOpen(prevIsMenuOpen => !prevIsMenuOpen);
+
+  const handleEdit = () => {
+    onEdit();
+    toggleMenu();
   };
 
-  toggleMenu = () => {
-    this.setState(prevState => ({ isMenuOpen: !prevState.isMenuOpen }));
+  const handleDelete = () => {
+    onDelete();
+    toggleMenu();
   };
 
-  handleEdit = () => {
-    this.props.onEdit();
-    this.toggleMenu();
-  };
+  return (
+    <>
+      <div className={s.container}>
+        <p>{text}</p>
+        <button className={s.menuButton} onClick={toggleMenu} aria-label="Menu">
+          <img src={randomIcon} alt="Menu" />
+        </button>
 
-  handleDelete = () => {
-    this.props.onDelete();
-    this.toggleMenu();
-  };
-
-  render() {
-    const { text } = this.props;
-    const { isMenuOpen } = this.state;
-
-    return (
-      <>
-        <div className={s.container}>
-          <p>{text}</p>
-          <button
-            className={s.menuButton}
-            onClick={this.toggleMenu}
-            aria-label="Menu"
-          >
-            <img src={randomIcon} alt="Menu" />
-          </button>
-          {isMenuOpen && (
-            <div className={s.menuStyles}>
-              <div className={s.menuItem} onClick={this.handleEdit}>
-                <span>
-                  <img className={s.menuItemImg} src={editIcon} alt="Edit" />
-                </span>
-                <span>Edit</span>
-              </div>
-
-              <div className={s.menuItem} onClick={this.handleDelete}>
-                <span>
-                  <img
-                    className={s.menuItemImg}
-                    src={deleteIcon}
-                    alt="Delete"
-                  />
-                </span>
-                <span>Delete</span>
-              </div>
+        {isMenuOpen && (
+          <div className={s.menuStyles}>
+            <div className={s.menuItem} onClick={handleEdit}>
+              <span>
+                <img className={s.menuItemImg} src={editIcon} alt="Edit" />
+              </span>
+              <span>Edit</span>
             </div>
-          )}
-        </div>
-      </>
-    );
-  }
-}
+
+            <div className={s.menuItem} onClick={handleDelete}>
+              <span>
+                <img className={s.menuItemImg} src={deleteIcon} alt="Delete" />
+              </span>
+              <span>Delete</span>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
 
 CardWithMenu.propTypes = {
   text: PropTypes.string.isRequired,
