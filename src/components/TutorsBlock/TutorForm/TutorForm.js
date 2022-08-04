2b03be9b82f077da148240ad430d143bb23cd69d
frandrew90 <frandrewjs@gmail.com>
2022-08-04 14:19:54 +0300

    refactoried tutorsForm

--- src/components/TutorsBlock/TutorForm/TutorForm.js
+++ src/components/TutorsBlock/TutorForm/TutorForm.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import BigButton from '../../common/BigButton/BigButton';
 import Paper from '../../common/Paper/Paper';
 import s from './TutorForm.module.css';
@@ -28,39 +28,20 @@ const GENDER = {
   FEMALE: 'female',
 };
 
-const INITIAL_STATE = {
-  firstName: '',
-  lastName: '',
-  patronymic: '',
-  gender: '',
-  phone: '',
-  email: '',
-  city: '',
-  options: '',
-  isFullTime: false,
-};
+const TutorForm = ({ onSubmit }) => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [patronymic, setPatronymic] = useState('');
+  const [gender, setGender] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [city, setCity] = useState('');
+  const [options, setOptions] = useState('');
+  const [isFullTime, setIsFullTime] = useState(false);
 
-class TutorForm extends Component {
-  state = { ...INITIAL_STATE };
-
-  handleChange = e => {
-    const { name, value, type, checked } = e.target;
-    const isCheckbox = type === 'checkbox';
-    this.setState({ [name]: isCheckbox ? checked : value });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    this.props.onSubmit({ ...this.state });
-    this.reset();
-  };
-
-  reset = () => {
-    this.setState({ ...INITIAL_STATE });
-  };
-
-  render() {
-    const {
+    onSubmit({
       firstName,
       lastName,
       patronymic,
@@ -70,153 +51,149 @@ class TutorForm extends Component {
       city,
       options,
       isFullTime,
-    } = this.state;
+    });
+    reset();
+  };
 
-    // const isAddBtnDisabled = Object.keys(this.state).some(inputName => {
-    //   if (inputName === 'isFullTime') {
-    //     return false;
-    //   }
-    //   return !this.state[inputName];
-    // });
+  const reset = () => {
+    setFirstName('');
+    setLastName('');
+    setPatronymic('');
+    setGender('');
+    setPhone('');
+    setEmail('');
+    setCity('');
+    setOptions('');
+    setIsFullTime(false);
+  };
 
-    // const isAddBtnDisabled = Object.keys(this.state).some(
-    //   inputName => inputName !== 'isFullTime' && !this.state[inputName],
-    // );
+  const requiredValues = [
+    firstName,
+    lastName,
+    patronymic,
+    gender,
+    phone,
+    email,
+    city,
+    options,
+  ];
+  const isSubmitBtnDisabled = requiredValues.some(value => !value);
 
-    const isAddBtnDisabled = Object.values(this.state).some(
-      value => typeof value !== 'boolean' && !value,
-    );
+  return (
+    <div className={s.container}>
+      <Paper>
+        <div className={s.inner}>
+          <h4 className="formName">Add Tutor</h4>
+          <form onSubmit={handleSubmit}>
+            <input
+              name="lastName"
+              value={lastName}
+              type="text"
+              placeholder="Last name*"
+              required
+              onChange={e => setLastName(e.target.value)}
+            />
+            <input
+              name="firstName"
+              value={firstName}
+              type="text"
+              placeholder="First name*"
+              required
+              onChange={e => setFirstName(e.target.value)}
+            />
+            <input
+              name="patronymic"
+              value={patronymic}
+              type="text"
+              placeholder="Patronymic*"
+              required
+              onChange={e => setPatronymic(e.target.value)}
+            />
+            <input
+              name="phone"
+              value={phone}
+              type="tel"
+              placeholder="Phone number*"
+              required
+              onChange={e => setPhone(e.target.value)}
+            />
+            <input
+              name="email"
+              value={email}
+              type="email"
+              placeholder="Email*"
+              required
+              onChange={e => setEmail(e.target.value)}
+            />
 
-    // const isAddBtnDisabled = Object.values(this.state).some(value => {
-    //   if (typeof value === 'boolean') {
-    //     return false;
-    //   }
-    //   return !value;
-    // });
+            <input
+              name="options"
+              value={options}
+              type="text"
+              placeholder="Kind of activity*"
+              required
+              onChange={e => setOptions(e.target.value)}
+            />
 
-    return (
-      <div className={s.container}>
-        <Paper>
-          <div className={s.inner}>
-            <h4 className="formName">Add Tutor</h4>
-            <form onSubmit={this.handleSubmit}>
-              <input
-                name="lastName"
-                value={lastName}
-                type="text"
-                placeholder="Last name*"
-                required
-                onChange={this.handleChange}
-              />
-              <input
-                name="firstName"
-                value={firstName}
-                type="text"
-                placeholder="First name*"
-                required
-                onChange={this.handleChange}
-              />
-              <input
-                name="patronymic"
-                value={patronymic}
-                type="text"
-                placeholder="Patronymic*"
-                required
-                onChange={this.handleChange}
-              />
-              <input
-                name="phone"
-                value={phone}
-                type="tel"
-                placeholder="Phone number*"
-                required
-                onChange={this.handleChange}
-              />
-              <input
-                name="email"
-                value={email}
-                type="email"
-                placeholder="Email*"
-                required
-                onChange={this.handleChange}
-              />
-              {/* <input
-                name="city"
-                // value={}
-                type="text"
-                placeholder="City*"
-                required
-                onChange={this.handleChange}
-              /> */}
-              <input
-                name="options"
-                value={options}
-                type="text"
-                placeholder="Kind of activity*"
-                required
-                onChange={this.handleChange}
-              />
+            <select
+              name="city"
+              value={city}
+              onChange={e => setCity(e.target.value)}
+              className={s.inner}
+            >
+              {citiesOptions.map(({ value, label }) => (
+                <option key={value} value={value}>
+                  {label}
+                </option>
+              ))}
+            </select>
 
-              <select
-                name="city"
-                value={city}
-                onChange={this.handleChange}
-                className={s.inner}
-              >
-                {citiesOptions.map(({ value, label }) => (
-                  <option key={value} value={value}>
-                    {label}
-                  </option>
-                ))}
-              </select>
-
-              <section>
-                <h5 className={s.inner}>Sex*</h5>
-                <div className={s.radioBtnWrapper}>
-                  <label className={s.inner}>Male</label>
-                  <input
-                    className={s.radioBtn}
-                    type="radio"
-                    checked={gender === GENDER.MALE}
-                    name="gender"
-                    value={GENDER.MALE}
-                    onChange={this.handleChange}
-                  />
-                  <label className={s.inner}>Female</label>
-                  <input
-                    className={s.radioBtn}
-                    type="radio"
-                    checked={gender === GENDER.FEMALE}
-                    name="gender"
-                    value={GENDER.FEMALE}
-                    onChange={this.handleChange}
-                  />
-                </div>
-              </section>
-
-              <div className={s.checkboxWrapper}>
-                <label className={s.inner}>Full time</label>
+            <section>
+              <h5 className={s.inner}>Sex*</h5>
+              <div className={s.radioBtnWrapper}>
+                <label className={s.inner}>Male</label>
                 <input
-                  className={s.checkbox}
-                  name="isFullTime"
-                  type="checkbox"
-                  checked={isFullTime}
-                  onChange={this.handleChange}
+                  className={s.radioBtn}
+                  type="radio"
+                  checked={gender === GENDER.MALE}
+                  name="gender"
+                  value={GENDER.MALE}
+                  onChange={e => setGender(e.target.value)}
+                />
+                <label className={s.inner}>Female</label>
+                <input
+                  className={s.radioBtn}
+                  type="radio"
+                  checked={gender === GENDER.FEMALE}
+                  name="gender"
+                  value={GENDER.FEMALE}
+                  onChange={e => setGender(e.target.value)}
                 />
               </div>
+            </section>
 
-              <BigButton
-                type="submit"
-                text="Invite"
-                disabled={isAddBtnDisabled}
+            <div className={s.checkboxWrapper}>
+              <label className={s.inner}>Full time</label>
+              <input
+                className={s.checkbox}
+                name="isFullTime"
+                type="checkbox"
+                checked={isFullTime}
+                onChange={e => setIsFullTime(e.target.checked)}
               />
-            </form>
-          </div>
-        </Paper>
-      </div>
-    );
-  }
-}
+            </div>
+
+            <BigButton
+              type="submit"
+              text="Invite"
+              disabled={isSubmitBtnDisabled}
+            />
+          </form>
+        </div>
+      </Paper>
+    </div>
+  );
+};
 
 TutorForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
