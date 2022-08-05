d287ca4f912465e13baff8eda65af37a73515389
frandrew90 <frandrewjs@gmail.com>
2022-08-05 16:00:10 +0300

    refactoried TutorsBlock

--- src/components/TutorsBlock/TutorsBlock.js
+++ src/components/TutorsBlock/TutorsBlock.js
@@ -1,6 +1,6 @@
 /** @jsxImportSource @emotion/react */
 
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import ErrorMsg from '../common/ErrorMsg/ErrorMsg';
 
 // import PropTypes from 'prop-types';
@@ -14,151 +14,139 @@ import * as api from '../../services/api';
 import addIcon from '../../images/plus.svg';
 import cancelIcon from '../../images/cancel-circle.svg';
 
-// const TutorsBlock = ({ tutors = [] }) => {
-//   return (
-//     <div css={{ position: 'relative', marginBottom: '32px' }}>
-//       <ul>
-//         {tutors.map(tutor => (
-//           <li key={tutor.email} css={{ marginBottom: '24px' }}>
-//             <Paper>
-//               <Tutor tutor={tutor} />
-//             </Paper>
-//           </li>
-//         ))}
-//       </ul>
-//       <BigButton icon={addIcon} text="Add Tutor" />
-//     </div>
-//   );
-// };
-
 const API_ENDPOINT = 'tutors';
 
-class TutorsBlock extends Component {
-  state = {
-    tutors: [],
-    isFormOpen: false,
-    newTutor: null,
-    loading: false,
-    error: null,
-    firstLoading: false,
+const TutorsBlock = () => {
+  const [tutors, setTutors] = useState([]);
+  const [isFormOpen, setIsFormOpen] = useState(false);
+  const [newTutor, setNewTutor] = useState(null);
+  // api request status
+  const [firstLoading, setFirstLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  //FETCH TUTORS
+
+  useEffect(() => {
+    const fetchTutors = async () => {
+      setFirstLoading(true);
+      setLoading(true);
+
+      try {
+        const tutors = await api.getData(API_ENDPOINT);
+        setTutors(tutors);
+        console.log(tutors);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setFirstLoading(false);
+        setLoading(false);
+      }
+    };
+
+    fetchTutors();
+  }, []);
+
+  //ADD TUTOR
+
+  useEffect(() => {
+    if (!newTutor) return;
+
+    const addTutor = async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        const savedTutor = await api.saveItem(API_ENDPOINT, newTutor);
+        setTutors(prevTutors => [...prevTutors, savedTutor]);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+        setIsFormOpen(false);
+        setNewTutor(null);
+      }
+    };
+    addTutor();
+  }, [newTutor]);
+
+  // //FETCH TUTORS with abortcontroller
+
+  // useEffect(() => {
+  //   const controller = new AbortController();
+  //   const signal = controller.signal;
+  //   const fetchTutors = async () => {
+  //     setFirstLoading(true);
+  //     setLoading(true);
+
+  //     try {
+  //       const tutors = await api.getData(API_ENDPOINT, { signal });
+  //       setTutors(tutors);
+  //       console.log(tutors);
+  //     } catch (error) {
+  //       if (!signal.aborted) {
+  //         setError(error.message);
+  //       }
+  //     } finally {
+  //       if (!signal.aborted) {
+  //         setFirstLoading(false);
+  //         setLoading(false);
+  //       }
+  //     }
+  //   };
+
+  //   fetchTutors();
+
+  //   return () => {
+  //     controller.abort();
+  //   };
+  // }, []);
+
+  // //ADD TUTOR with light version of abortcontroller
+
+  // useEffect(() => {
+  //   if (!newTutor) return;
+
+  //   let isTutorsMounted = true;
+  //   const addTutor = async () => {
+  //     setLoading(true);
+  //     setError(null);
+  //     try {
+  //       const savedTutor = await api.saveItem(API_ENDPOINT, newTutor);
+  //       if (isTutorsMounted) {
+  //         setTutors(prevTutors => [...prevTutors, savedTutor]);
+  //       }
+  //     } catch (error) {
+  //       if (isTutorsMounted) {
+  //         setError(error.message);
+  //       }
+  //     } finally {
+  //       if (isTutorsMounted) {
+  //         setLoading(false);
+  //         setIsFormOpen(false);
+  //         setNewTutor(null);
+  //       }
+  //     }
+  //   };
+  //   addTutor();
+
+  //   return () => {
+  //     isTutorsMounted = false;
+  //   };
+  // }, [newTutor]);
+
+  const toggleForm = () => {
+    setIsFormOpen(prevIsFormOpen => !prevIsFormOpen);
   };
 
-  // Для предотвращения утечки памяти при размонтировании компонента(простой вариант) - пример в AddTutor:
-  // isTutorsMounted = false;
-  // Для предотвращения утечки памяти при размонтировании компонента(сложный вариант) - пример в FetchTutor:
-  // controller = new AbortController();
-  // signal = this.controller.signal;
+  const noTutors = !firstLoading && !tutors.length;
 
-  componentDidMount() {
-    // this.isTutorsMounted = true;
-    this.setState({ firstLoading: true });
-    this.fetchTutors().finally(() => this.setState({ firstLoading: false }));
-  }
+  return (
+    <div>
+      {loading && <Loader />}
 
-  componentDidUpdate(prevProps, prevState) {
-    const { newTutor } = this.state;
-    if (newTutor && prevState.newTutor !== newTutor) {
-      this.addTutor();
-    }
-  }
+      {firstLoading && <Skeleton />}
 
-  componentWillUnmount() {
-    // this.isTutorsMounted = false;
-    // if (this.controller) {
-    //   this.controller.abort();
-    // }
-  }
-
-  fetchTutors = async () => {
-    // this.createSignalAndController();
-    // const signal = { signal: this.signal };
-    this.setState({ loading: true, error: null });
-
-    try {
-      const tutors = await api.getData(
-        API_ENDPOINT,
-        // signal
-      );
-
-      this.setState({ tutors });
-    } catch (error) {
-      // if (!this.signal.aborted) {
-      this.setState({ error: error.message });
-      // }
-    } finally {
-      // if (!this.signal.aborted) {
-      this.setState({ loading: false });
-      // }
-    }
-  };
-
-  toggleForm = () => {
-    this.setState(prevState => ({
-      isFormOpen: !prevState.isFormOpen,
-    }));
-  };
-
-  confirmAdd = newTutor => {
-    this.setState({ newTutor });
-  };
-
-  // addTutor = async newTutor => {
-  //   // this.setState(prevState => ({
-  //   //   tutors: [...prevState.tutors, newTutor],
-  //   //   isFormOpen: false,
-  //   // }));
-
-  //   const savedTutor = await api.saveItem(API_ENDPOINT, newTutor);
-
-  //   this.setState(prevState => ({
-  //     tutors: [...prevState.tutors, savedTutor],
-  //     isFormOpen: false,
-  //   }));
-  // };
-  addTutor = async () => {
-    this.setState({ loading: true, error: null });
-    const { newTutor } = this.state;
-    try {
-      const savedTutor = await api.saveItem(API_ENDPOINT, newTutor);
-
-      // if (this.isTutorsMounted) {
-      this.setState(prevState => ({
-        tutors: [...prevState.tutors, savedTutor],
-        isFormOpen: false,
-        newTutor: null,
-      }));
-      // }
-    } catch (error) {
-      // if (this.isTutorsMounted) {
-      this.setState({ error: error.message });
-      // }
-    } finally {
-      // if (this.isTutorsMounted) {
-      this.setState({ loading: false });
-      // }
-    }
-  };
-
-  // Для предотвращения утечки памяти при размонтировании компонента(сложный вариант) - пример в FetchTutor(продолжение):
-  // createSignalAndController = () => {
-  //   if (this.controller) {
-  //     this.controller.abort();
-  //   }
-  //   this.controller = new AbortController();
-  //   this.signal = this.controller.signal;
-  // };
-
-  render() {
-    const { tutors, isFormOpen, loading, error, firstLoading } = this.state;
-
-    const noTutors = !firstLoading && !tutors.length;
-
-    return (
-      <div>
-        {loading && <Loader />}
-
-        {firstLoading && <Skeleton />}
-
+      {!!tutors.length && (
         <ul>
           {tutors.map(tutor => (
             <li key={tutor.id} css={{ marginBottom: '24px' }}>
@@ -168,36 +156,22 @@ class TutorsBlock extends Component {
             </li>
           ))}
         </ul>
+      )}
 
-        {noTutors && <h4> No tutors yet!</h4>}
+      {noTutors && <h4> No tutors yet!</h4>}
 
-        {isFormOpen && <TutorForm onSubmit={this.confirmAdd} />}
+      {isFormOpen && <TutorForm onSubmit={setNewTutor} />}
 
-        {error && <ErrorMsg message={error} />}
+      {error && <ErrorMsg message={error} />}
 
-        <BigButton
-          icon={isFormOpen ? cancelIcon : addIcon}
-          text={isFormOpen ? 'Cancel adding' : 'Add Tutor'}
-          onClickBtn={this.toggleForm}
-          disabled={loading}
-        />
-      </div>
-    );
-  }
-}
-
-// TutorsBlock.propTypes = {
-//   tutors: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       firstName: PropTypes.string,
-//       lastName: PropTypes.string,
-//       patronymic: PropTypes.string,
-//       phone: PropTypes.string,
-//       email: PropTypes.string,
-//       city: PropTypes.string,
-//       options: PropTypes.string,
-//     }),
-//   ).isRequired,
-// };
+      <BigButton
+        icon={isFormOpen ? cancelIcon : addIcon}
+        text={isFormOpen ? 'Cancel adding' : 'Add Tutor'}
+        onClickBtn={toggleForm}
+        disabled={loading}
+      />
+    </div>
+  );
+};
 
 export default TutorsBlock;
