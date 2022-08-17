1fde2b866b3ce58c40771c0b154b7011b72cf997
frandrew90 <frandrewjs@gmail.com>
2022-08-17 13:22:26 +0300

    fixed themes styles

--- src/components/common/Filter/Filter.js
+++ src/components/common/Filter/Filter.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useContext } from 'react';
+import { ThemeContext, themes } from '../../../context/themeContext';
 import s from './Filter.module.css';
 
 const Filter = ({ label = '', onFilterChange, value }) => {
+  const { theme } = useContext(ThemeContext);
   return (
     <div className={s.container}>
       <label>
@@ -11,6 +14,11 @@ const Filter = ({ label = '', onFilterChange, value }) => {
           type="text"
           value={value}
           onChange={e => onFilterChange(e.target.value)}
+          className={
+            theme === themes.light
+              ? s.filterInput
+              : `${s.filterInput} ${s.filterInputDark}`
+          }
         />
       </label>
     </div>
