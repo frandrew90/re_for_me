a7ced0ba523bacf836a53a6fa9c3792bfa334c35
frandrew90 <frandrewjs@gmail.com>
2022-08-19 15:53:58 +0300

    refactoried citiesSlice from separated to combine

--- src/redux/cities/citiesSlice.js
+++ src/redux/cities/citiesSlice.js
@@ -1,35 +1,76 @@
-import { createSlice, combineReducers } from '@reduxjs/toolkit';
+//COMBINE SLICE FOR EACH PIECE OF STATE
 
-const itemsSlice = createSlice({
-  name: 'items',
-  initialState: [],
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  items: [],
+  filter: '',
+};
+
+const citiesSlice = createSlice({
+  name: 'cities',
+  initialState,
   reducers: {
-    setCities: (_, { payload }) => payload,
+    setCities: (state, { payload }) => ({ ...state, items: payload }),
 
-    addCity: (state, { payload }) => [...state, payload],
+    addCity: (state, { payload }) => ({
+      ...state,
+      items: [...state.items, payload],
+    }),
 
-    editCity: (state, { payload }) =>
-      state.map(city => (city.id === payload.id ? payload : city)),
+    editCity: (state, { payload }) => ({
+      ...state,
+      items: state.items.map(city => (city.id === payload.id ? payload : city)),
+    }),
 
-    deleteCity: (state, { payload }) =>
-      state.filter(city => city.id !== payload),
+    deleteCity: (state, { payload }) => ({
+      ...state,
+      items: state.items.filter(city => city.id !== payload),
+    }),
+
+    changeFilter: (state, { payload }) => ({ ...state, filter: payload }),
   },
 });
 
-const filterSlice = createSlice({
-  name: 'filter',
-  initialState: '',
-  reducers: {
-    changeFilter: (_, { payload }) => payload,
-  },
-});
+export const { setCities, addCity, editCity, deleteCity, changeFilter } =
+  citiesSlice.actions;
 
-export const { setCities, addCity, editCity, deleteCity } = itemsSlice.actions;
-export const { changeFilter } = filterSlice.actions;
+export default citiesSlice.reducer;
 
-const citiesReducer = combineReducers({
-  [itemsSlice.name]: itemsSlice.reducer,
-  [filterSlice.name]: filterSlice.reducer,
-});
+//SEPARATED SLICES FROM EACH PIECE OF STATE
 
-export default citiesReducer;
+// import { createSlice, combineReducers } from '@reduxjs/toolkit';
+
+// const itemsSlice = createSlice({
+//   name: 'items',
+//   initialState: [],
+//   reducers: {
+//     setCities: (_, { payload }) => payload,
+
+//     addCity: (state, { payload }) => [...state, payload],
+
+//     editCity: (state, { payload }) =>
+//       state.map(city => (city.id === payload.id ? payload : city)),
+
+//     deleteCity: (state, { payload }) =>
+//       state.filter(city => city.id !== payload),
+//   },
+// });
+
+// const filterSlice = createSlice({
+//   name: 'filter',
+//   initialState: '',
+//   reducers: {
+//     changeFilter: (_, { payload }) => payload,
+//   },
+// });
+
+// export const { setCities, addCity, editCity, deleteCity } = itemsSlice.actions;
+// export const { changeFilter } = filterSlice.actions;
+
+// const citiesReducer = combineReducers({
+//   [itemsSlice.name]: itemsSlice.reducer,
+//   [filterSlice.name]: filterSlice.reducer,
+// });
+
+// export default citiesReducer;
