02e22ad322a242e0dbe6c917711aee0b227b4c13
frandrew90 <frandrewjs@gmail.com>
2022-08-19 13:57:23 +0300

    refactoried tutorsBlock on redux/toolkit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,21 @@
-import { createStore, combineReducers } from 'redux';
-import { devToolsEnhancer } from 'redux-devtools-extension';
+import { configureStore } from '@reduxjs/toolkit';
+import { createLogger } from 'redux-logger';
 import citiesReducer from './cities/citiesReducer';
 import tutorsReducer from './tutors/tutorsReducer';
 
-const rootReducer = combineReducers({
-  tutors: tutorsReducer,
-  cities: citiesReducer,
-  departments: () => [],
+const logger = createLogger({
+  collapsed: (getState, action, logEntry) => !logEntry.error,
+  timestamp: false,
 });
 
-const store = createStore(rootReducer, devToolsEnhancer());
+const store = configureStore({
+  reducer: {
+    tutors: tutorsReducer,
+    cities: citiesReducer,
+    departments: () => [],
+  },
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger),
+  devTools: process.env.NODE_ENV !== 'production',
+});
 
 export default store;
